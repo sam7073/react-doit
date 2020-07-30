0e6ad3fc30fdfa1782419e3e60e7d6a3c38fdcef
unknown <sam7073@naver.com>
2020-07-30 09:13:47 +0900

    Updates

---
+++ precache-manifest.a13c7e88c2196820a1faf6edaae6d153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "212c1713763711e07b10",
+    "url": "/react-doit/static/css/main.b37ef742.chunk.css"
+  },
+  {
+    "revision": "212c1713763711e07b10",
+    "url": "/react-doit/static/js/main.8dcbed42.chunk.js"
+  },
+  {
+    "revision": "cb2b3fc9b5c533942939",
+    "url": "/react-doit/static/js/runtime~main.669637e1.js"
+  },
+  {
+    "revision": "1868546b76755d13629b",
+    "url": "/react-doit/static/js/2.cdefec29.chunk.js"
+  },
+  {
+    "revision": "81c5c00ac572a8f73ecbdb76bd86a6e3",
+    "url": "/react-doit/index.html"
+  }
+];
